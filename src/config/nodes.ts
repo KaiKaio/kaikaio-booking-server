22cc72b54d590167d8ee0a145e0e61c114aae3dd
Kaiwei <kaikaiano4@gmail.com>
2026-06-29 16:59:25 +0800

    fix; 更新设备列表

--- src/config/nodes.ts
+++ src/config/nodes.ts
@@ -1,16 +1,50 @@
 export default [
   {
-    id: 'mac',
-    name: 'MacBook',
-    // ip: '10.242.46.156',
-    ip: '10.242.76.134',
-    // ip: '192.168.1.149',
+    id: 'MacBookPro',
+    name: 'MacBookPro',
+    ip: '10.242.46.156',
     port: 22,
   },
   {
-    id: 'windows',
-    name: 'Windows-PC',
+    id: 'Ace3vTierFix',
+    name: 'Ace3vTierFix',
+    ip: '10.242.208.39',
+    port: 22,
+  },
+  {
+    id: 'KaiX570',
+    name: 'KaiX570',
     ip: '10.242.78.83',
+    port: 22,
+  },
+  {
+    id: 'Surface',
+    name: 'Surface',
+    ip: '10.242.18.33',
+    port: 22,
+  },
+  {
+    id: 'MacBookAir',
+    name: 'MacBookAir',
+    ip: '10.242.21.36',
+    port: 22,
+  },
+  {
+    id: 'Kai_Danzhou',
+    name: 'Kai_Danzhou',
+    ip: '10.242.122.214',
+    port: 22,
+  },
+  {
+    id: '14Pro',
+    name: '14Pro',
+    ip: '10.242.162.233',
+    port: 22,
+  },
+  {
+    id: 'iPad_Mini4',
+    name: 'iPad_Mini4',
+    ip: '10.242.115.176',
     port: 22,
   },
 ];
