b254ea018764ab2550d001d92cf5ad0e38ea9afd
Kaiwei <kaikaiano4@gmail.com>
2026-07-13 11:27:17 +0800

    fix: 设备探测全部改为 PING 方式

--- src/config/nodes.ts
+++ src/config/nodes.ts
@@ -14,7 +14,7 @@ const nodes: NodeConfig[] = [
     id: 'MacBookPro',
     name: 'MacBookPro',
     ip: '10.242.46.156',
-    probe: { type: 'tcp', port: 22 },
+    probe: { type: 'ping' },
   },
   {
     id: 'Ace3vTierFix',
@@ -26,25 +26,25 @@ const nodes: NodeConfig[] = [
     id: 'KaiX570',
     name: 'KaiX570',
     ip: '10.242.78.83',
-    probe: { type: 'tcp', port: 22 },
+    probe: { type: 'ping' },
   },
   {
     id: 'Surface',
     name: 'Surface',
     ip: '10.242.18.33',
-    probe: { type: 'tcp', port: 22 },
+    probe: { type: 'ping' },
   },
   {
     id: 'MacBookAir',
     name: 'MacBookAir',
     ip: '10.242.21.36',
-    probe: { type: 'tcp', port: 22 },
+    probe: { type: 'ping' },
   },
   {
     id: 'Kai_Danzhou',
     name: 'Kai_Danzhou',
     ip: '10.242.122.214',
-    probe: { type: 'tcp', port: 22 },
+    probe: { type: 'ping' },
   },
   {
     id: '14Pro',
